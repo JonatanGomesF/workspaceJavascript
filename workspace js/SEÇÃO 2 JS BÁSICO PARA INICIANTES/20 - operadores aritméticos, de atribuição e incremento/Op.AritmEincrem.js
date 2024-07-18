947669f5cd678047fc3d54d76a58a58e764285d2
jonatan gomes floriano <160983484+jonatancolt@users.noreply.github.com>
2024-07-18 09:44:06 -0300

    Add files via upload

---
+++ workspace js/SEÇÃO 2 JS BÁSICO PARA INICIANTES/20 - operadores aritméticos, de atribuição e incremento/Op.AritmEincrem.js
@@ -0,0 +1,186 @@
+//aritméticos + - / *   -->  básicos
+//Adição e concatenação (- / +)
+//potenciação (**)
+//resto da divisão (%)
+//____________________________________________________________________
+
+/* parseInt (inteiro), parseFloat(decimais)
+
+const num1 = parseFloat('10.6'); // resultado 10.6 (com todas as casas decimais)
+const num2 = 'jonatan';// resultado string
+const num3 = parseInt('5.2'); //resultado 5 (inteiro)
+const num4 = Number('4.8') //JS irá se virar e dizer se é inteiro ou decimal
+
+console.log(num1 * num2); //NaN (not a number NÃO PODEMOS MISTURAR STRINGS E NUMBERS)*/
+
+
+
+
+
+
+
+//________________________________________________________________________________
+
+
+
+
+
+
+
+
+/*OPERADORES DE ATRIBUIÇÃO (POTENCIALIZANDO)
+let contador = 2;
+contador **= 10;
+contador **= 10;
+contador **= 10;
+console.log(contador);
+
+OPERADORES DE ATRIBUIÇÃO (MULTIPLICANDO)
+let contador = 0;
+contador *= 2;
+contador *= 2;
+contador *= 2;
+console.log(contador);
+
+OPERADORES DE ATRIBUIÇÃO (SOMANDO)
+let contador = 0;
+contador += 2;
+contador += 2;
+contador += 2;
+console.log(contador);
+
+
+
+
+
+
+
+
+
+_____________________________________________________________________________________
+
+
+
+
+
+
+
+/*const passo = 4;
+let contador = 0;
+
+contador = contador + passo;
+console.log(contador);
+contador = contador + passo;
+console.log(contador);
+contador = contador + passo;
+console.log(contador);*/
+
+
+
+
+
+
+
+
+
+//________________________________________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*operador de decremento
+let jonatan = 1;
+jonatan--; // 2
+jonatan--; // 3
+jonatan--; // 4
+jonatan--; // 5
+console.log(jonatan);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*operador de incremento
+let jonatan = 1;
+jonatan++; // 2
+jonatan++; // 3
+jonatan++; // 4
+jonatan++; // 5
+console.log(jonatan);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const num1 = 5;
+const num2 = 10;
+const num3 = 2;
+console.log((num1 + num2) * num3); 
+
+NOTA: SÓ POSSO ATRIBUIR DOIS OPERADORES ARITMETICOS (SOMA E DIVISÃO) 
+COLOCANDO SEPARADAMENTE CADA OPERADOR ENTRE () EX ((NUM1 + NUM2) * NUM3)
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2); //exemplo de adição (+)
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 - num2); //exemplo de subtração (-)
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 / num2); //exemplo de divisão (/)
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 * num2); //exemplo de multiplicação (*)
+
+const num1 = 2;
+const num2 = 10;
+console.log(num1 ** num2); //exemplo de potenciação (**)
+const num1 = 10;
+const num2 = 3;
+console.log(num1 % num2); //exemplo de resto de divisão (%)
+*/
