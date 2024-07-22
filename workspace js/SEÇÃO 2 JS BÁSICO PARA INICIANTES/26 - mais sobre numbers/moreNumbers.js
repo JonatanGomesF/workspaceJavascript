8a359b6856c34481ece3446035d8f00384b381be
jonatan gomes floriano <160983484+jonatancolt@users.noreply.github.com>
2024-07-21 22:22:55 -0300

    Add files via upload

---
+++ workspace js/SEÇÃO 2 JS BÁSICO PARA INICIANTES/26 - mais sobre numbers/moreNumbers.js
@@ -0,0 +1,9 @@
+let num1 = 10; //number
+let num2 = 2.5; //number
+
+
+//console.log(num1.toString() + num2);
+//console.log(num1.toString(2)); //representação binária
+console.log(num1.toFixed(2)); //arredondamento de casas decimais
+//console.log(Number.isInteger(num1)); //esse "isInteger" busca saber se é verdadeiro ou false 
+
